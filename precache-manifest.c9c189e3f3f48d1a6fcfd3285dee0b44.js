ef3c37ac08850b26ac1d10d5a948c921b0db2221
Matias <matias.rl@hotmail.com>
2021-09-06 22:09:16 -0300

    Updates

---
+++ precache-manifest.c9c189e3f3f48d1a6fcfd3285dee0b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2102253ea616b8689ec4aff631fe93a2",
+    "url": "/react-heroesApp/index.html"
+  },
+  {
+    "revision": "7bb21eccce9233c4a1a3",
+    "url": "/react-heroesApp/static/css/main.82e341ac.chunk.css"
+  },
+  {
+    "revision": "0823e5d932a8416d2bb2",
+    "url": "/react-heroesApp/static/js/2.8482b70a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-heroesApp/static/js/2.8482b70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb21eccce9233c4a1a3",
+    "url": "/react-heroesApp/static/js/main.0b39648d.chunk.js"
+  },
+  {
+    "revision": "f1d7c1a10dae86f8e085",
+    "url": "/react-heroesApp/static/js/runtime-main.399f508b.js"
+  }
+]);
